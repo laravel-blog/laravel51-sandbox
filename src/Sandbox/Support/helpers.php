9fe0652ecf3d0451051698cc5db650892941e814
Stefan Riedel | laravelblog <sr@laravel-blog.de>
2015-07-31 11:22:19 +0200

    allowed and is_guest functions added

--- src/Sandbox/Support/helpers.php
+++ src/Sandbox/Support/helpers.php
@@ -5,3 +5,16 @@
  * Time: 17:57
  * Project: sandbox
  */
+
+function allowed($sAction = null,$sRole = null) {
+    return \Helper::allowed($sAction, $sRole);
+}
+
+/**
+ * to check user is a guest
+ *
+ * @return bool
+ */
+function is_guest() {
+    return !\Auth::check();
+}
